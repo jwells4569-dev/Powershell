8b348293defc890300707d97707a313196b3ef8f
Powershell <jwells4569@gmail.com>
2019-08-14 08:01:40 -0700

    Bulk Mailer for Tickets

---
+++ UpdatesLast120.ps1
@@ -0,0 +1,7 @@
+﻿$Hosts = Get-Content -Path 'C:\hosts.txt'
+Invoke-Command -ComputerName $Hosts -ScriptBlock {
+    Get-HotFix | Where-Object {
+        $_.InstalledOn -gt ((Get-Date).AddDays(-120))
+    } | Select-Object -Property PSComputerName, Description, HotFixID, InstalledOn
+} | Format-Table -AutoSize |
+Out-File -Encoding ASCII -FilePath 'C:\Recent_OS_Updates.txt' -Append -ErrorAction SilentlyContinue
