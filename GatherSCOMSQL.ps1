600a955c2c9b2f92d90fb21375b311f8238ef37e
Powershell <jwells4569@gmail.com>
2019-08-28 12:18:06 -0700

    Updates

--- GatherSCOMSQL.ps1
+++ GatherSCOMSQL.ps1
@@ -1,1 +1,2 @@
-﻿Invoke-Sqlcmd -InputFile "C:\HighPriSCOM.sql" | Export-Csv -Path "C:\HighPriSCOM.csv" -NoTypeInformation
+﻿Write-Host "Scraping SCOM SQL for Harddrive Issues" -ForegroundColor Gray
+Invoke-Sqlcmd -InputFile "C:\HighPriSCOM.sql" | Export-Csv -Path "C:\HighPriSCOM.csv" -NoTypeInformation
